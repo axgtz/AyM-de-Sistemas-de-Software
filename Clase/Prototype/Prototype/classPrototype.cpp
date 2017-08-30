ee7788ca496eefbe95236e338438d820bf0059b4
axgtz <axguti@gmail.com>
2017-08-30 08:48:03 -0500

    Added puml removed hpp

--- Clase/Prototype/Prototype/classPrototype.cpp
+++ Clase/Prototype/Prototype/classPrototype.cpp
@@ -5,11 +5,116 @@
 //  Copyright © 2017 Alejandro Gutierrez. All rights reserved.
 //
 
-#include "classPrototype.hpp"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
+class Mundo {
+public:
+    Mundo(string mundo) : nombreMundo(mundo){}
+    string nombreMundo;
+};
+
+
 class Personaje {
 public:
-    virtual Personaje* clone();
+    Personaje() = default;
+    Personaje(string nombre) : nombre(nombre){}
+    string nombre;
+    int telefono;
+    Mundo* mundo;
+    string correo;
+    virtual Personaje* clone() = 0;
+};
+
+
+class Princesa : public Personaje{
+public:
+    Princesa(string n) : Personaje(n){}
+    
+    Princesa(const Princesa& p){
+        nombre = p.nombre;
+        telefono = p.telefono;
+        correo = p.correo;
+        mundo = new Mundo(p.mundo ->nombreMundo); //Se crea nueva instancia de mundo con el mismo nombre del munod original
+    }
+    
+    void grita(){
+        cout << "Ayyyy Ayudaa!!!"<<endl;};
+    
+    Personaje* clone(){
+        return new Princesa(*this);
+    }
+};
+
+
+class Villano : public Personaje{
+public:
+    Villano(string n) : Personaje(n){}
+    
+    Villano(const Villano& p){
+        nombre = p.nombre;
+        telefono = p.telefono;
+        correo = p.correo;
+        mundo = new Mundo(p.mundo ->nombreMundo);
+    }
+    
+    void secuestra(){
+        cout << "U r mine hahahahah"<< endl;}
+    Personaje* clone(){
+        return this;
+    }
+    
+};
+
+
+class Heroe : public Personaje{
+public:
+    Heroe(string n) : Personaje(n){}
+    
+    Heroe(const Heroe& p){//copy contructor
+        nombre = p.nombre;
+        telefono = p.telefono;
+        correo = p.correo;
+        mundo = new Mundo(p.mundo ->nombreMundo);
+    }
+    
+    void grita(){
+        cout << "Te voy a salvar bella princesa"<<endl;};
+    Personaje* clone(){
+        return this;
+    }
+};
+
+
+int main(int argc, const char * argv[]) {
+   
+    Princesa* prin = new Princesa("Rapunzel");
+    prin->correo = "yolo@swag.com";
+    prin->telefono = 553233232;
+    
+    Princesa* p = dynamic_cast<Princesa*>(prin->clone());
+    prin->nombre = "RAPUNZEL";
+    prin->mundo->nombreMundo = "Paletón";
+
+    cout << p->nombre <<endl;
+    cout << p->correo <<endl;
+    cout << p->telefono <<endl;
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
